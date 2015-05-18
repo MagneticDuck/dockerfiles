eacb8c2e2cd8007b14d140634a6566481de6a89a
Magnetic Duck <zenmags@gmail.com>
2015-05-18 15:13:33 +0000

    added new stuff

---
+++ launchers/chrome.sh
@@ -0,0 +1,1 @@
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $HOME/Downloads:/root/Downloads -v $HOME/.config/google-chrome/:/data -v /dev/snd:/dev/snd --privileged --name chrome jess/chrome
